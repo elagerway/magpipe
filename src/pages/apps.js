bdea6712a23ea3d337e63db74acc9f93bd027d27
Erik Lagerway <elagerway@gmail.com>
2026-01-29 17:39:16 -0800

    Remove 'Settings' text from back button on Apps page

--- src/pages/apps.js
+++ src/pages/apps.js
@@ -39,7 +39,6 @@ export default class AppsPage {
           <svg width="20" height="20" fill="none" stroke="currentColor" viewBox="0 0 24 24">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"/>
           </svg>
-          Settings
         </button>
 
         <h1 style="margin-bottom: 1.5rem;">Apps</h1>
